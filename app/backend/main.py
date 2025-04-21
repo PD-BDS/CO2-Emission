974fcd93ab2a2d634f976449c94875ec1610549e
PD-BDS <dey.piyal97@gmail.com>
2025-04-21 19:34:10 +0200

    update

--- app/backend/main.py
+++ app/backend/main.py
@@ -32,20 +32,24 @@ def get_last_24h_emissions():
 @app.get("/next-6h-predictions")
 def get_next_6h_predictions():
     return query_db("""
-        SELECT TimeStamp, Prediction FROM predictions
-        WHERE TimeStamp > datetime('now')
-        ORDER BY TimeStamp
+        SELECT DISTINCT TimeStamp, Prediction
+        FROM predictions
+        WHERE Actual IS NULL
+        ORDER BY TimeStamp ASC
         LIMIT 6
     """)
+
 
 @app.get("/last-6h-predictions-vs-actual")
 def get_last_6h_predictions_vs_actual():
     return query_db("""
-        SELECT TimeStamp, Prediction, Actual FROM predictions
-        WHERE TimeStamp <= datetime('now')
+        SELECT DISTINCT TimeStamp, Prediction, Actual
+        FROM predictions
+        WHERE Actual IS NOT NULL
         ORDER BY TimeStamp DESC
         LIMIT 6
     """)
+
 
 @app.get("/best-model")
 def get_latest_model():
